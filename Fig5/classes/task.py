5b01fcb8e92dbd25b11a803f904848e506e5537d
Ulises Pereira <pimuntue@gmail.com>
2024-07-01 14:05:55 -0700

    new commit fig 5

---
+++ Fig5/classes/task.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+class working_memory_task(object):
+    def __init__(self, modelparams,  connectivity):
+        self.N = modelparams['N_block']
+        self.n_ctx =  modelparams['n_ctx'] 
+        self.p = connectivity.hebbian_symmetric.p
+        self.ind_stim = []
+        self.t_start = modelparams['t_start']
+        self.period = modelparams['period'] 
+        self.t_stim = modelparams['t_stim']
+        
+        #pattern external input
+        self.patterns_sym = connectivity.hebbian_symmetric.patterns_current_sym
+        self.patterns_asym = connectivity.hebbian_symmetric.patterns_current_asym
+        self.pattern_rand = np.random.binomial(1, .5,self.N* self.n_ctx)
+        self.g_p = modelparams['g_p']
+        self.i_pat = np.zeros(self.N * self.n_ctx)
+        self.ind_asym = 0
+        self.pat_input()
+        
+
+    #input pattern hebbian 
+    def pat_input(self, ind_pat=0):
+        for l in self.ind_stim:
+            ind1 = l * self.N #index start
+            ind2 = (l + 1) * self.N#index end
+            stim = self.patterns_asym[l, :, ind_pat, self.ind_asym] #pattern
+            self.i_pat[ind1:ind2] = self.g_p * stim
+     #input pattern hebbian 
+    def pat_input_random(self):
+        for l in self.ind_stim:
+            ind1 = l * self.N #index start
+            ind2 = (l + 1) * self.N#index end
+            self.i_pat[ind1:ind2] = self.g_p * self.pattern_rand[ind1:ind2]
+    
+    def input_current(self,t):
+        ''' input current '''
+        if  self.t_start <= t and  t <= self.t_start + self.t_stim:
+            return self.i_pat
+        else:   
+            return np.zeros(self.N * self.n_ctx)
+
